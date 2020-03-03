cfb58f35cce31ed4c0f53df9dc7cdba8bb247df1
Rick Verkuijlen <rickverkuijlen@hotmail.nl>
2020-03-03 21:27:13 +0100

    Created the rest application

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Starts the REST server
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"controllers"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
